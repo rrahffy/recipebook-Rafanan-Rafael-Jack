3fdbe9a20439321356e05e1923b79f6805057511
rrahffy <rafa.jac.rjr@gmail.com>
2026-03-02 23:06:52 +0800

    Implemented the Recipe detail protection and author display

--- ledger/views.py
+++ ledger/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 
 def recipes_list(request):
@@ -6,7 +7,8 @@ def recipes_list(request):
     context = {'recipes': recipes}
     return render(request, 'recipe_list.html', context)
 
+@login_required
 def recipe(request, pk):
-    recipe = Recipe.objects.get(pk=pk)
+    recipe = get_object_or_404(Recipe, pk=pk)
     context = {'recipe': recipe}
     return render(request, 'recipe.html', context)
