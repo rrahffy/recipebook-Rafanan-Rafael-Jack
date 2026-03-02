5316bf1e86ed20e0214e3b2d67ccce62cdc782e9
rrahffy <rafa.jac.rjr@gmail.com>
2026-03-02 23:21:00 +0800

    Configuring auth redirects and template directories

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'recipebook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,3 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+LOGIN_REDIRECT_URL = 'recipes_list'  # Redirect to Recipe List after login
+LOGOUT_REDIRECT_URL = 'login'         # Redirect to login page after logout
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
