7b6ecebf2e3a77946c80ba081c96476da2ffef60
rrahffy <rafa.jac.rjr@gmail.com>
2026-03-12 16:07:49 +0800

    registered Profile model in admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient, Profile
+
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
