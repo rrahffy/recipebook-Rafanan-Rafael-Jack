2b95764227ca1e79578eb480a912eb024d937b30
rrahffy <rafa.jac.rjr@gmail.com>
2026-03-12 19:32:21 +0800

    added the URLs for recipe add and image upload

--- ledger/urls.py
+++ ledger/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('recipes/list', views.recipes_list, name='recipes_list'),
     path('recipe/<int:pk>', views.recipe, name='recipe'),
+    path('recipe/add', views.RecipeCreateView.as_view(), name='recipe_add'),
+    path('recipe/<int:pk>/add_image', views.RecipeImageCreateView.as_view(), name='recipe_add_image'),
 ]
